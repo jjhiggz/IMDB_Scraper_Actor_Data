24c9f83e5ef10546f3973ea1a251a79ec4441244
jjhiggz <jonathan.higger@gmail.com>
2020-07-08 00:00:58 -0600

    added a way to save json's for each actor

--- create_file.py
+++ create_file.py
@@ -1,7 +1,7 @@
-def create_actor_file( actor_data ):
-  import json
-  import os
-  filename = actor_data["name"] + '_data.json'
-  os.chdir('actors_data')
-  with open(filename,"w") as f:
-    f.write(json.dumps( actor_data ))
+  def create_actor_file( actor_data ):
+    import json
+    import os
+    filename = actor_data["name"] + '_data.json'
+    os.chdir('actors_data')
+    with open(filename,"w") as f:
+      f.write(json.dumps( actor_data ))
